29c32039819b819b2bf8e9fe1b1bc47c5f1a9961
suni1234 <sunilkumar.kakani11@gmail.com>
2025-04-10 11:59:07 +0530

    first commit

---
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.javatechie.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsApplication {
+
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@javax.annotation.PostConstruct
+	public void init() {
+		logger.info("Application Started");
+	}	
+
+	public static void main(String[] args) {
+		logger.info("Application Excuted");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
